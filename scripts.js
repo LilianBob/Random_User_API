4915bd15912244b55a61f72a4d3909d761c02f0a
lilian Robert <mslillianito@gmail.com>
2021-04-07 16:37:53 -0700

    displayed json response on html table

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.btn').click(function(){
+        $.get("https://randomuser.me/api/?results=20", function(res){
+            $('.randomUsers thead').html(`
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Email</th>
+                <th>Address</th>
+            </tr>`)
+            var user;
+            $.each(res.results, function(i, value){
+                user = '<tr>' + '<td>' + value.name.first + + value.name.last + 
+                '</td>'+ '<td>' + value.dob.age + '</td>' + '<td>' + value.email + 
+                '</td>'+ '<td>' + value.location + '</td>' + '</tr>'
+                $(user).appendTo('.randomUsers tbody');
+            });
+            
+        }, "json");
+    })
+})
